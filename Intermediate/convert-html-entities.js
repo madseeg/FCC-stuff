a151f44d07d05d6a488beeb320c4237e059ce2db
Utmost excellent mobile reception <air.drive@gmail.com>
2016-04-22 16:34:53 -0500

    Create convert-html-entities.js
    Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.

---
+++ Intermediate/convert-html-entities.js
@@ -0,0 +1,5 @@
+function convertHTML(str) {
+ return str.replace(/\&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/\'/g, "&apos;").replace(/\"/g, "&quot;");   
+}
+
+convertHTML("Dolce & Gabbana");
